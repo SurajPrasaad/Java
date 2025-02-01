c0c6729293646db119e1d9c757601ef356717533
SurajPrasaad <surajkameshwarprasad@gmail.com>
2025-02-01 16:26:34 +0530

    Variable Example in java

--- VaribaleExample.java
+++ VaribaleExample.java
@@ -3,10 +3,12 @@ public class VaribaleExample {
         int integerValue =10;
         float floatValue = 3.64534754375347f;
         double doubleValue = 35632.6434765372523554;
+        byte byteValue = 127;
+        boolean booleanValue = true;
         char charValue = 'S';
         String stringValue ="Suraj Kumar";
 
-        System.out.println("Integer Value :: "+integerValue+"\nFloat Value :: "+floatValue+"\nDouble Value :: "+doubleValue+"\nCharacter Value :: "+charValue+"\nString Value :: "+stringValue);
+        System.out.println("Integer Value :: "+integerValue+"\nFloat Value :: "+floatValue+"\nDouble Value :: "+doubleValue+"\nByte Value :: "+byteValue+"\nBoolean Value :: "+booleanValue+"\nCharacter Value :: "+charValue+"\nString Value :: "+stringValue);
 
     }
 }
