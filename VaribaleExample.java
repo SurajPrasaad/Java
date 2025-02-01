2b9ddc05cd515dfd1033a6b94eac59566d67af79
SurajPrasaad <surajkameshwarprasad@gmail.com>
2025-02-01 16:51:17 +0530

    Variable Example in java

--- VaribaleExample.java
+++ VaribaleExample.java
@@ -1,14 +1,16 @@
-public class VaribaleExample {
-    public static void main(String[] args) {
-        int integerValue =10;
-        float floatValue = 3.64534754375347f;
-        double doubleValue = 35632.6434765372523554;
-        byte byteValue = 127;
-        boolean booleanValue = true;
-        char charValue = 'S';
-        String stringValue ="Suraj Kumar";
+    public class VaribaleExample {
+        public static void main(String[] args) {
+            int integerValue =2147483647; //int data range ::  -2147483648 to 2147483647
+            float floatValue = 3.40282347e+38f;//float data range :: 1.40239846e-45f to 3.40282347e+38f
+            double doubleValue = 1.79769313486231570e+308;// double data range :: 4.94065645841246544e-324 to 1.79769313486231570e+308
+            byte byteValue = 127;//byte data range ::  -128 to 127
+            boolean booleanValue = true;//boolean data range :: 0 (false) or 1 (true)
+            char charValue = 65535;//char data range :: 0 ('\u0000') to 65535 ('\uffff')
+            long longValue = 922337203685477580L;//long data range :: -9223372036854775808 to 9223372036854775807
+            short shortValue =  32767;//-32768 to 32767
+            String stringValue ="Suraj Kumar";
 
-        System.out.println("Integer Value :: "+integerValue+"\nFloat Value :: "+floatValue+"\nDouble Value :: "+doubleValue+"\nByte Value :: "+byteValue+"\nBoolean Value :: "+booleanValue+"\nCharacter Value :: "+charValue+"\nString Value :: "+stringValue);
+            System.out.println("Integer Value :: "+integerValue+"\nFloat Value :: "+floatValue+"\nDouble Value :: "+doubleValue+"\nByte Value :: "+byteValue+"\nBoolean Value :: "+booleanValue+"\nCharacter Value :: "+charValue+"\nString Value :: "+stringValue+"\nlong Value :: "+longValue+"\nShort Value :: "+shortValue);
 
+        }
     }
-}
